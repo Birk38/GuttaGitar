dda3781b88f64e3a57e4dfefa4568c5249aae21d
Birk38 <152275425+Birk38@users.noreply.github.com>
2023-12-02 22:36:01 +0000

    1. Footer ferdig 2. Responsiv ferdig 3. småfix

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -33,7 +33,7 @@ function toggleAutoScroll() {
 startButton.addEventListener('click', toggleAutoScroll);
 
 // dynamisk scroll event listner
-scrollSpeedInput.addEventListener('input', function() {
+scrollSpeedInput.addEventListener('input', function () {
     // Update scrollSpeed with the current input value
     scrollSpeed = parseInt(scrollSpeedInput.value, 10) || 25;
 
@@ -46,11 +46,11 @@ scrollSpeedInput.addEventListener('input', function() {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
- 
+}
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+document.addEventListener('DOMContentLoaded', function () {
     // Function to check if the page is scrolled to the bottom
     function isScrolledToBottom() {
         const scrollPosition = window.scrollY || window.pageYOffset;
@@ -61,18 +61,48 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Event listener for the scroll event
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         if (isScrolledToBottom()) {
-            sleep(1000).then(() => { scrolling=false
+            sleep(2000).then(() => {
+                scrolling = false
                 autoscrollText.style.animation = 'none';
                 scrollSpeed = 0;
-                clearTimeout(scrolldelay); });
-            
-            
+                clearTimeout(scrolldelay);
+            });
+
+
             // The page is scrolled to the bottom
             console.log('Scrolled to the bottom!');
-            
+
             // Your code to handle reaching the bottom of the page
         }
     });
 });
+//FJERNER AUTOSCROLL SECTION OVER FOOTER
+// Get the elements you want to manipulate
+var elementToHide = document.getElementById('Autoscroll_section');
+var referenceElement = document.getElementById('Grid_Sang');
+
+// Add a scroll event listener
+window.onscroll = function () {
+    // Get the offset top of the reference element
+    var referenceElementOffset = referenceElement.offsetTop;
+
+    // Get the height of the reference element
+    var referenceElementHeight = referenceElement.offsetHeight;
+
+    // Calculate the hide point to be the sum of offset top and height of the reference element
+    var hidePoint = referenceElementOffset + referenceElementHeight;
+
+    // Get the bottom position of the viewport
+    var bottomPosition = window.innerHeight + window.scrollY;
+
+    // Check if the bottom position is beyond the hidePoint
+    if (bottomPosition > hidePoint) {
+        // If it is, hide the element
+        elementToHide.style.display = 'none';
+    } else {
+        // If not, show the element
+        elementToHide.style.display = 'block';
+    }
+};
